9e5923688c2a527e14d4f1782f25a33947938ac4
Renato Aparecido de Oliveira <raoliveira@daitan.com>
2018-07-17 21:48:55 -0300

    Adding splash scene and start menu

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void changeScene(int sceneIndex) {
+		SceneManager.LoadScene(sceneIndex);
+	}
+}
---
+++ Assets/_Scripts/SplashScreen.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+	private Image splashImage;
+	private LevelManager lm;
+
+	#region Custom Methods
+	void closeSplashScene() {
+		lm.changeScene(1);
+	}
+
+	void getComponentsFindObjects() {
+		splashImage = GetComponent<Image>();
+		lm = GameObject.Find("Level Manager").GetComponent<LevelManager>();
+	}
+
+	void invokeMethods() {
+		Invoke("makeOpaque", 1.5f);
+		Invoke("closeSplashScene", 3.5f);
+	}
+
+	void makeOpaque() {
+		splashImage.color = new Color(1f, 1f, 1f, 1f);
+	}
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		getComponentsFindObjects();
+		invokeMethods();
+	}
+}
