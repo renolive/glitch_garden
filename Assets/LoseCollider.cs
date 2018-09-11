00ec29e7766da1d0aa79d00788f0f5bd9e537d81
Renato Aparecido de Oliveira <raoliveira@daitan.com>
2018-09-11 18:02:18 -0300

    adding lose trigger

---
+++ Assets/LoseCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+	private LevelManager lm;
+
+	#region CustomMethods
+	void getComponentsAndObjects ()
+	{
+		lm = GameObject.Find("Level Manager").GetComponent<LevelManager>();
+	}
+	#endregion
+
+	void Start () {
+		getComponentsAndObjects();	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.GetComponent<Attacker>()) {
+			lm.changeScene("3b_LoseScene");
+		}
+	}
+
+	void OnDrawGizmos () {
+		BoxCollider2D box = GetComponent<BoxCollider2D>();
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube (transform.position, box.size);
+	}
+}
